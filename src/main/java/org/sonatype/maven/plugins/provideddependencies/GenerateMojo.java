0227d067dddfaf82e3fa275f5f228fa936edc0ab
Marvin Froeder <velo@sonatype.com>
2010-09-13 12:49:59 +0000

    Changed the way this plugin resolves the exclusions
    git-svn-id: file:///opt/svn/repositories/sonatype.org/plugins/trunk/provided-dependencies-maven-plugin@504 c2a14038-686d-4adc-8740-a14e5738cb64

--- src/main/java/org/sonatype/maven/plugins/provideddependencies/GenerateMojo.java
+++ src/main/java/org/sonatype/maven/plugins/provideddependencies/GenerateMojo.java
@@ -3,20 +3,14 @@ package org.sonatype.maven.plugins.provideddependencies;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.ArtifactUtils;
 import org.apache.maven.artifact.factory.ArtifactFactory;
-import org.apache.maven.artifact.resolver.filter.AndArtifactFilter;
-import org.apache.maven.artifact.resolver.filter.ArtifactFilter;
-import org.apache.maven.artifact.resolver.filter.ExcludesArtifactFilter;
-import org.apache.maven.artifact.resolver.filter.IncludesArtifactFilter;
+import org.apache.maven.artifact.repository.ArtifactRepository;
 import org.apache.maven.model.Dependency;
 import org.apache.maven.model.DependencyManagement;
 import org.apache.maven.model.Exclusion;
@@ -26,7 +20,12 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.MavenProjectBuilder;
+import org.apache.maven.project.ProjectBuildingException;
 import org.codehaus.plexus.util.IOUtil;
+
+import com.google.common.collect.LinkedHashMultimap;
+import com.google.common.collect.Multimap;
 
 /**
  * @author velo
@@ -39,24 +38,14 @@ public class GenerateMojo
 {
 
     /**
-     * @parameter default-value="${project.groupId}"
+     * @component
      */
-    private String groupId;
+    private ArtifactFactory artifactFactory;
 
     /**
      * @parameter default-value="${project.artifactId}-dependencies"
      */
     private String artifactId;
-
-    /**
-     * @parameter default-value="${project.artifactId}-compile"
-     */
-    private String compileDependenciesArtifactId;
-
-    /**
-     * @parameter default-value="${project.version}"
-     */
-    private String version;
 
     /**
      * @parameter default-value="${project.artifacts}"
@@ -65,10 +54,17 @@ public class GenerateMojo
     private Collection<Artifact> artifacts;
 
     /**
-     * @parameter default-value="${project.build.directory}"
-     * @readonly
+     * @parameter default-value="${project.artifactId}-compile"
      */
-    private File target;
+    private String compileDependenciesArtifactId;
+
+    /**
+     * @parameter default-value="${project.groupId}"
+     */
+    private String groupId;
+
+    /** @parameter expression="${localRepository}" */
+    private ArtifactRepository localRepository;
 
     /**
      * @parameter default-value="${project}"
@@ -79,11 +75,70 @@ public class GenerateMojo
     /**
      * @component
      */
-    private ArtifactFactory artifactFactory;
+    private MavenProjectBuilder projectBuilder;
+
+    /** @parameter expression="${project.remoteArtifactRepositories}" */
+    private List<? extends ArtifactRepository> remoteRepositories;
+
+    /**
+     * @parameter default-value="${project.build.directory}"
+     * @readonly
+     */
+    private File target;
+
+    /**
+     * @parameter default-value="${project.version}"
+     */
+    private String version;
+
+    private void appendExclusions( Multimap<String, String> exclusions, List<Dependency> dependencies )
+    {
+        for ( Dependency dependency : dependencies )
+        {
+            List<Exclusion> excls = dependency.getExclusions();
+            if ( !excls.isEmpty() )
+            {
+                for ( Exclusion excl : excls )
+                {
+                    exclusions.put(
+                        ArtifactUtils.versionlessKey( dependency.getGroupId(), dependency.getArtifactId() ),
+                        ArtifactUtils.versionlessKey( excl.getGroupId(), excl.getArtifactId() ) );
+                }
+            }
+        }
+    }
+
+    private Multimap<String, String> collectExclusions()
+    {
+        Multimap<String, String> exclusions = LinkedHashMultimap.create();
+        for ( Artifact artifact : artifacts )
+        {
+            MavenProject p;
+            try
+            {
+                p = projectBuilder.buildFromRepository( artifact, remoteRepositories, localRepository );
+            }
+            catch ( ProjectBuildingException e )
+            {
+                // ignore
+                continue;
+            }
+
+            appendExclusions( exclusions, p.getDependencies() );
+            if ( p.getDependencyManagement() != null )
+            {
+                appendExclusions( exclusions, p.getDependencyManagement().getDependencies() );
+            }
+        }
+
+        return exclusions;
+    }
 
     public void execute()
         throws MojoExecutionException, MojoFailureException
     {
+
+        Multimap<String, String> exclusions = collectExclusions();
 
         Model pom = new Model();
         pom.setModelVersion( "4.0.0" );
@@ -93,15 +148,58 @@ public class GenerateMojo
         pom.setPackaging( "pom" );
 
         DependencyManagement dependencyManagement = new DependencyManagement();
-        dependencyManagement.setDependencies( getDependencies( Artifact.SCOPE_PROVIDED ) );
+        dependencyManagement.setDependencies( getDependencies( Artifact.SCOPE_PROVIDED, exclusions ) );
         pom.setDependencyManagement( dependencyManagement );
 
         persist( pom, "-dependencies.pom" );
 
         pom.setDependencyManagement( null );
-        pom.setDependencies( getDependencies( Artifact.SCOPE_COMPILE ) );
+        pom.setDependencies( getDependencies( Artifact.SCOPE_COMPILE, exclusions ) );
         pom.setArtifactId( compileDependenciesArtifactId );
         persist( pom, "-compile.pom" );
+    }
+
+    private List<Dependency> getDependencies( String scope, Multimap<String, String> exclusions )
+    {
+        List<Dependency> dependencies = new ArrayList<Dependency>();
+        for ( Artifact artifact : artifacts )
+        {
+            Dependency dep = new Dependency();
+            dep.setGroupId( artifact.getGroupId() );
+            dep.setArtifactId( artifact.getArtifactId() );
+            dep.setVersion( artifact.getBaseVersion() );
+            dep.setClassifier( artifact.getClassifier() );
+            dep.setType( artifact.getType() );
+
+            if ( Artifact.SCOPE_TEST.equals( artifact.getScope() )
+                || Artifact.SCOPE_SYSTEM.equals( artifact.getScope() )
+                || Artifact.SCOPE_IMPORT.equals( artifact.getScope() ) )
+            {
+                // dep.setScope( Artifact.SCOPE_TEST );
+                continue;
+            }
+            else
+            {
+                dep.setScope( scope );
+            }
+
+            Collection<String> excls = exclusions.get( ArtifactUtils.versionlessKey( artifact ) );
+            for ( String exclusion : excls )
+            {
+                String[] pattern = exclusion.split( ":" );
+                if ( pattern.length == 2 )
+                {
+                    Exclusion ex = new Exclusion();
+                    ex.setGroupId( pattern[0] );
+                    ex.setArtifactId( pattern[1] );
+                    dep.addExclusion( ex );
+                }
+            }
+
+            dependencies.add( dep );
+        }
+
+        return dependencies;
     }
 
     private void persist( Model pom, String suffix )
@@ -128,109 +226,4 @@ public class GenerateMojo
         project.addAttachedArtifact( artifact );
     }
 
-    private List<Dependency> getDependencies( String scope )
-    {
-        List<Dependency> dependencies = new ArrayList<Dependency>();
-        for ( Artifact artifact : artifacts )
-        {
-            Dependency dep = new Dependency();
-            dep.setGroupId( artifact.getGroupId() );
-            dep.setArtifactId( artifact.getArtifactId() );
-            dep.setVersion( artifact.getBaseVersion() );
-            dep.setClassifier( artifact.getClassifier() );
-            dep.setType( artifact.getType() );
-
-            if ( Artifact.SCOPE_TEST.equals( artifact.getScope() )
-                || Artifact.SCOPE_SYSTEM.equals( artifact.getScope() )
-                || Artifact.SCOPE_IMPORT.equals( artifact.getScope() ) )
-            {
-                // dep.setScope( Artifact.SCOPE_TEST );
-                continue;
-            }
-            else
-            {
-                dep.setScope( scope );
-            }
-
-            // using a set to prevent duplicated entries
-            Set<String> exclusions = new LinkedHashSet<String>( getExclusions( artifact.getDependencyFilter() ) );
-            for ( String exclusion : exclusions )
-            {
-                String[] pattern = exclusion.split( ":" );
-                if ( pattern.length == 2 )
-                {
-                    Exclusion ex = new Exclusion();
-                    ex.setGroupId( pattern[0] );
-                    ex.setArtifactId( pattern[1] );
-                    dep.addExclusion( ex );
-                }
-            }
-
-            dependencies.add( dep );
-        }
-
-        return dependencies;
-    }
-
-    private Collection<String> getExclusions( ArtifactFilter filter )
-    {
-        if ( filter == null )
-        {
-            return Collections.emptyList();
-        }
-
-        if ( filter instanceof ExcludesArtifactFilter )
-        {
-            ExcludesArtifactFilter efilter = (ExcludesArtifactFilter) filter;
-            return getPatterns( efilter );
-        }
-        if ( filter instanceof AndArtifactFilter )
-        {
-            AndArtifactFilter af = (AndArtifactFilter) filter;
-            Collection<ArtifactFilter> filters = getFilters( af );
-            List<String> patterns = new ArrayList<String>();
-            for ( ArtifactFilter f : filters )
-            {
-                patterns.addAll( getExclusions( f ) );
-            }
-
-            return patterns;
-        }
-
-        getLog().warn( "Unsupported filter " + filter.getClass() );
-
-        return Collections.emptyList();
-    }
-
-    @SuppressWarnings( "unchecked" )
-    private Collection<ArtifactFilter> getFilters( AndArtifactFilter af )
-    {
-        try
-        {
-            Field f = AndArtifactFilter.class.getDeclaredField( "filters" );
-            f.setAccessible( true );
-            return (Collection<ArtifactFilter>) f.get( af );
-        }
-        catch ( Exception e )
-        {
-            getLog().error( e.getMessage() );
-            return Collections.emptyList();
-        }
-    }
-
-    @SuppressWarnings( "unchecked" )
-    private Collection<String> getPatterns( ExcludesArtifactFilter efilter )
-    {
-        try
-        {
-            Field f = IncludesArtifactFilter.class.getDeclaredField( "patterns" );
-            f.setAccessible( true );
-            return (Collection<String>) f.get( efilter );
-        }
-        catch ( Exception e )
-        {
-            getLog().error( e.getMessage() );
-            return Collections.emptyList();
-        }
-    }
 }
